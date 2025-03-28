5f736803a29abf1e54883a92016a60393a7c9979
eiliya <eiliya.abedini@gmail.com>
2025-03-28 15:06:25 +0000

    Update PlanetList Screen to pass the test

--- app/src/main/kotlin/ir/iact/starwarsplanets/presentation/PlanetListScreen.kt
+++ app/src/main/kotlin/ir/iact/starwarsplanets/presentation/PlanetListScreen.kt
@@ -1,10 +1,21 @@
 package ir.iact.starwarsplanets.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ir.iact.starwarsplanets.R
 import ir.iact.starwarsplanets.domain.model.Planet
 
 // Planet Compose Screen that shows list of planets using lazy column
@@ -13,8 +24,55 @@ fun PlanetListScreen(
     modifier: Modifier = Modifier,
     planets: List<Planet>
 ) {
-
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(planets.size) { index ->
+            val planet = planets[index]
+            PlanetItem(
+                planet = planet,
+                modifier = Modifier
+            )
+        }
+    }
 }
+
+@Composable
+fun PlanetItem(
+    planet: Planet,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = planet.name,
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(R.string.climate),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(text = planet.climate, style = MaterialTheme.typography.bodyLarge)
+            }
+            Text(
+                text = stringResource(R.string.population, planet.population),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
