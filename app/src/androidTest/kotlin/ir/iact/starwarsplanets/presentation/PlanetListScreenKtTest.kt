11171bb8c3c845eaaaa62ad99d0a1fa55683f803
eiliya <eiliya.abedini@gmail.com>
2025-03-29 19:03:31 +0000

    Add planet list screen tests for loading state, error state, retry button and on item click

--- app/src/androidTest/kotlin/ir/iact/starwarsplanets/presentation/PlanetListScreenKtTest.kt
+++ app/src/androidTest/kotlin/ir/iact/starwarsplanets/presentation/PlanetListScreenKtTest.kt
@@ -1,9 +1,17 @@
 package ir.iact.starwarsplanets.presentation
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onParent
+import androidx.compose.ui.test.performClick
 import ir.iact.starwarsplanets.domain.model.Planet
+import ir.iact.starwarsplanets.presentation.planetlist.PlanetListContract
+import ir.iact.starwarsplanets.presentation.planetlist.PlanetListContract.UiState
+import ir.iact.starwarsplanets.presentation.planetlist.PlanetListScreen
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -13,7 +21,7 @@ class PlanetListScreenKtTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun testPlanetItem() {
+    fun showPlanetListWhenDataAvailable() {
         val planet = Planet(
             name = "Earth",
             climate = "Tropical",
@@ -27,7 +35,11 @@ class PlanetListScreenKtTest {
 
         composeTestRule.setContent {
             PlanetListScreen(
-                planets = listOf(planet, planet2),
+                state = UiState(
+                    planets = listOf(planet, planet2),
+                    isLoading = false
+                ),
+                onUiInteraction = {}
             )
         }
 
@@ -38,5 +50,105 @@ class PlanetListScreenKtTest {
         composeTestRule.onNodeWithText("Population: 7000000000").assertIsDisplayed()
         composeTestRule.onNodeWithText("Population: 0").assertIsDisplayed()
     }
+
+    @Test
+    fun showLoadingWhenDataIsLoading() {
+        val planet = Planet(
+            name = "Earth",
+            climate = "Tropical",
+            population = 7_000_000_000
+        )
+        composeTestRule.setContent {
+            PlanetListScreen(
+                state = UiState(
+                    planets = listOf(planet),
+                    isLoading = true
+                ),
+                onUiInteraction = {}
+            )
+        }
+
+        composeTestRule.onNodeWithTag("loading").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Earth").assertIsNotDisplayed()
+    }
+
+    @Test
+    fun showErrorWithRetryButtonWhenDataHasError() {
+        val planet = Planet(
+            name = "Earth",
+            climate = "Tropical",
+            population = 7_000_000_000
+        )
+        composeTestRule.setContent {
+            PlanetListScreen(
+                state = UiState(
+                    planets = listOf(planet),
+                    isLoading = false,
+                    hasError = "Error happened while fetching data"
+                ),
+                onUiInteraction = {}
+            )
+        }
+
+        composeTestRule.onNodeWithText("Error happened while fetching data").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Retry").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("loading").assertIsNotDisplayed()
+        composeTestRule.onNodeWithText("Earth").assertIsNotDisplayed()
+    }
+
+    @Test
+    fun onRetryClickedShouldTriggerUiInteractionForRetry() {
+        val planet = Planet(
+            name = "Earth",
+            climate = "Tropical",
+            population = 7_000_000_000
+        )
+        var retryButtonClicked: Boolean = false
+        composeTestRule.setContent {
+            PlanetListScreen(
+                state = UiState(
+                    planets = listOf(planet),
+                    isLoading = false,
+                    hasError = "Error happened while fetching data"
+                ),
+                onUiInteraction = {
+                    if (it is PlanetListContract.UiInteraction.OnRetryClicked) {
+                        retryButtonClicked = true
+                    }
+                }
+            )
+        }
+
+        composeTestRule.onNodeWithText("Retry").performClick()
+        assertEquals(true, retryButtonClicked)
+    }
+
+    @Test
+    fun onItemClickedTriggerUiInteractionForPlanetSelected() {
+        val planet = Planet(
+            name = "Earth",
+            climate = "Tropical",
+            population = 7_000_000_000
+        )
+        var selectedPlanet: Planet? = null
+        composeTestRule.setContent {
+            PlanetListScreen(
+                state = UiState(
+                    planets = listOf(planet),
+                    isLoading = false,
+                    hasError = "Error happened while fetching data"
+                ),
+                onUiInteraction = {
+                    if (it is PlanetListContract.UiInteraction.OnPlanetClicked) {
+                        selectedPlanet = it.planet
+                    }
+                }
+            )
+        }
+
+        composeTestRule.onNodeWithText("Earth").onParent().performClick()
+        assertEquals(planet, selectedPlanet)
+    }
+
 
 }
