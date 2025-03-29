a8a80a1903145d9a6132f9f3a79089eff43656a2
eiliya <eiliya.abedini@gmail.com>
2025-03-29 20:25:49 +0000

    Create test for PlanetDetailScreen

--- app/src/androidTest/kotlin/ir/iact/starwarsplanets/presentation/PlanetDetailScreenKtTest.kt
+++ app/src/androidTest/kotlin/ir/iact/starwarsplanets/presentation/PlanetDetailScreenKtTest.kt
@@ -1,17 +1,10 @@
 package ir.iact.starwarsplanets.presentation
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onParent
-import androidx.compose.ui.test.performClick
-import ir.iact.starwarsplanets.domain.model.Planet
-import ir.iact.starwarsplanets.presentation.planetlist.PlanetListContract
-import ir.iact.starwarsplanets.presentation.planetlist.PlanetListContract.UiState
-import ir.iact.starwarsplanets.presentation.planetlist.PlanetListScreen
-import org.junit.Assert.assertEquals
+import ir.iact.starwarsplanets.presentation.planetdetail.PlanetDetailContract.UiState
+import ir.iact.starwarsplanets.presentation.planetdetail.PlanetDetailScreen
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,131 +14,20 @@ class PlanetDetailScreenKtTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun showPlanetListWhenDataAvailable() {
-        val planet = Planet(
-            name = "Earth",
-            climate = "Tropical",
-            population = 7_000_000_000
-        )
-        val planet2 = Planet(
-            name = "Mars",
-            climate = "Cold",
-            population = 0
-        )
-
+    fun showPlanetDetailItems() {
         composeTestRule.setContent {
-            PlanetListScreen(
+            PlanetDetailScreen(
                 state = UiState(
-                    planets = listOf(planet, planet2),
-                    isLoading = false
-                ),
-                onUiInteraction = {}
+                    planet = PlanetData.planet3,
+                )
             )
         }
 
-        composeTestRule.onNodeWithText("Earth").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Mars").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Tropical").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Cold").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Population: 7000000000").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Population: 0").assertIsDisplayed()
-    }
-
-    @Test
-    fun showLoadingWhenDataIsLoading() {
-        val planet = Planet(
-            name = "Earth",
-            climate = "Tropical",
-            population = 7_000_000_000
-        )
-        composeTestRule.setContent {
-            PlanetListScreen(
-                state = UiState(
-                    planets = listOf(planet),
-                    isLoading = true
-                ),
-                onUiInteraction = {}
-            )
-        }
-
-        composeTestRule.onNodeWithTag("loading").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Earth").assertIsNotDisplayed()
-    }
-
-    @Test
-    fun showErrorWithRetryButtonWhenDataHasError() {
-        val planet = Planet(
-            name = "Earth",
-            climate = "Tropical",
-            population = 7_000_000_000
-        )
-        composeTestRule.setContent {
-            PlanetListScreen(
-                state = UiState(
-                    planets = listOf(planet),
-                    isLoading = false,
-                    hasError = "Error happened while fetching data"
-                ),
-                onUiInteraction = {}
-            )
-        }
-
-        composeTestRule.onNodeWithText("Error happened while fetching data").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Retry").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("loading").assertIsNotDisplayed()
-        composeTestRule.onNodeWithText("Earth").assertIsNotDisplayed()
-    }
-
-    @Test
-    fun onRetryClickedShouldTriggerUiInteractionForRetry() {
-        val planet = Planet(
-            name = "Earth",
-            climate = "Tropical",
-            population = 7_000_000_000
-        )
-        var retryButtonClicked: Boolean = false
-        composeTestRule.setContent {
-            PlanetListScreen(
-                state = UiState(
-                    planets = listOf(planet),
-                    isLoading = false,
-                    hasError = "Error happened while fetching data"
-                ),
-                onUiInteraction = {
-                    if (it is PlanetListContract.UiInteraction.OnRetryClicked) {
-                        retryButtonClicked = true
-                    }
-                }
-            )
-        }
-
-        composeTestRule.onNodeWithText("Retry").performClick()
-        assertEquals(true, retryButtonClicked)
-    }
-
-    @Test
-    fun onItemClickedTriggerUiInteractionForPlanetSelected() {
-        val planet = Planet(
-            name = "Earth",
-            climate = "Tropical",
-            population = 7_000_000_000
-        )
-        var selectedPlanet: Planet? = null
-        composeTestRule.setContent {
-            PlanetListScreen(
-                state = UiState(
-                    planets = listOf(planet),
-                    isLoading = false,
-                ),
-                onUiInteraction = {
-                    if (it is PlanetListContract.UiInteraction.OnPlanetClicked) {
-                        selectedPlanet = it.planet
-                    }
-                }
-            )
-        }
-
-        composeTestRule.onNodeWithText("Earth").onParent().performClick()
-        assertEquals(planet, selectedPlanet)
+        composeTestRule.onNodeWithText("Tatooine").assertIsDisplayed()
+        composeTestRule.onNodeWithText("arid").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Population: 200000").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Diameter: 10465").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Gravity: 1 standard").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Terrain: desert").assertIsDisplayed()
     }
 }
